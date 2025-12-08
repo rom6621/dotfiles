827514bd3ef8cdc96ac6596094047c716c2b9d80
rom6621 <3103rom@gmail.com>
2025-12-08 13:02:48 +0900

    Treesitterの設定を追加

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,27 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"lua",
-			"go",
-		},
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+	install = function()
+		require("nvim-treesitter.install").setup({
+			prefer_git = false,
+			compilers = { "gcc" }
+		})
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = false,
+			ensure_installed = {
+				"lua",
+				"markdown",
+				"html",
+				"javascript",
+				"typescript",
+				"go",
+				"terraform",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
 
